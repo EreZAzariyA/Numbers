e61a49b276c6066f2d7fef8fc527ecc8f0523c65
Erez Azariya <erezazariya@gmail.com>
2024-12-07 21:53:24 +0200

    credit-cards page

--- src/components/credit-cards/index.tsx
+++ src/components/credit-cards/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Result } from "antd";
+import { Button, Flex, Result, Typography } from "antd";
 import { useTranslation } from "react-i18next";
 
 
@@ -6,10 +6,8 @@ const CreditCards = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="page-container credit-cards">
-      <div className="title-container">
-        <div className="page-title">{t('pages.creditCards')}</div>
-      </div>
+    <Flex vertical className="page-container credit-cards">
+      <Typography.Title level={2} className="page-title">{t('pages.creditCards')}</Typography.Title>
       <div className="page-inner-container">
       <Result
         status="500"
@@ -19,7 +17,7 @@ const CreditCards = () => {
       />
       </div>
 
-    </div>
+    </Flex>
   );
 };
 
